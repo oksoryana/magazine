7f30dbdb26ef0dfc92302dbc60a103530fcc3930
Oksana <oksoryana@gmail.com>
2018-06-07 23:50:34 +0300

    новый вариант

--- Lesson2/js/main.js
+++ Lesson2/js/main.js
@@ -17,7 +17,7 @@ let mainList = {
 
 //мой способ 1
 
-for (let i=0; i<2; i++)
+for (let i=0; i<3; i++)
 {
 	mainList.shopGoods [i] = prompt ("какой тип товаров будем продавать?");
 	mainList.price [i] = prompt ("Цена товара");
@@ -29,54 +29,14 @@ do
 {
 	mainList.shopGoods [num] = prompt ("какой тип товаров будем продавать?");
 	num++; 
-} while (num<5);
+} while (num<3);
 */
 
 // мой способ 3
 /*var num=0;
-while (num<5)
+while (num<3)
 {
 	mainList.shopGoods [num] = prompt ("какой тип товаров будем продавать?");
 	num++;
 }
 */
-
-
-// вариант 1
-function showmainList(){
-	var bud = mainList.budget/30;
-	alert(bud);
-}
-showmainList();
-*/
-// вариант 2
-
-/*function showDiskount () {
-		let x = prompt (mainList.diskount = "есть дисконт?");
-		if (x=)
-	alert(mainList.diskount);
-}
-showDiskount();
-*/
-//	if x = 1 {
-//		console.log('mainList.shopGoods'*30%)
-//	}
-//	else
-//		console.log('mainList.shopGoods'*0%)
-
-//}
-
-function showmainList(){
-		alert(mainList.budget/30);
-}
-showmainList();
-
-
-
-
-
-
-
-
-
-
